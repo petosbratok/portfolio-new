8e697f3b740d2e6e5976a4ab785bcefd043286a3
petosbratok <44753312+petosbratok@users.noreply.github.com>
2022-10-16 15:27:33 +0300

    Revert "Animation on page redirect"
    This reverts commit 33c17860acb2ea4d77431d44599d692b39d2db33.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,3 @@ function scrollSmoothTo(elementId) {
   var element = document.getElementById(elementId);
   element.scrollIntoView({ block: 'start',  behavior: 'smooth' });
 }
-
-function delay (URL) {
-    $("body").css("opacity", "0")
-    setTimeout( function() { window.location = URL }, 200 );
-}
