33c17860acb2ea4d77431d44599d692b39d2db33
petosbratok <44753312+petosbratok@users.noreply.github.com>
2022-10-16 15:20:35 +0300

    Animation on page redirect

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ function scrollSmoothTo(elementId) {
   var element = document.getElementById(elementId);
   element.scrollIntoView({ block: 'start',  behavior: 'smooth' });
 }
+
+function delay (URL) {
+    $("body").css("opacity", "0")
+    setTimeout( function() { window.location = URL }, 200 );
+}
